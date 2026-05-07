dda5311df6a0e441d8f9eb39d32d066e5da77237
Ariel Sinaga <arielsinaga67@gmail.com>
2026-05-07 11:19:03 +0700

    Minor Changes on DB Config

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ official_image_index = {}
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT"),
+    "port": int(os.getenv("DB_PORT")),
     "database": os.getenv("DB_NAME"),
     "user": os.getenv("DB_USER"),
     "password": os.getenv("DB_PASSWORD")
